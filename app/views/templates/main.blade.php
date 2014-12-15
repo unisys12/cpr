365bb52fe67a91973113dd6b7dfe7010c1f6357c
Phillip Jackson <unisys12@gmail.com>
2014-12-15 07:26:03 -0600

    update telephone href with link mixin

--- app/views/templates/main.blade.php
+++ app/views/templates/main.blade.php
@@ -73,7 +73,7 @@
   			<h3>Contact Us</h3>
   			<ul>
 	  			<li>Office Hours: Monday - Friday 8am - 5pm</li>
-	  			<li>Phone: <a href="tel://(662)286-3067">(662)286-3067</a></li>
+	  			<li>Phone: <a href="tel://(662)286-3067" class="tele">(662)286-3067</a></li>
 	  			<li>Fax: (662)286-1310</li>
 	  			<li>Mailing Address: P.O. Box 1372, Corinth, MS 38835</li>
 	  			<li>Shipping Address: 309 S. Parkway St, Corinth, MS 38834</li>
