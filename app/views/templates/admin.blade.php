7f04a4ba0d1e9af9532172fbf688e4b80521176f
Phillip Jackson <unisys12@gmail.com>
2014-12-15 13:23:05 -0600

    add backend admin template

---
+++ app/views/templates/admin.blade.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html class="no-js" lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>@yield('title')</title>
+		<link rel="stylesheet" href="/css/style.css" />
+		<script src="/packages/tinymce/tinymce.min.js"></script>
+		<script>
+			//tinymce.baseURL = "../public/packages";
+			//tinymce.suffix = '.min';
+			tinymce.init(
+				{
+					relative_urls: false,
+					plugins: "link jbimage",
+					toolbar: "link | jbimages",
+					selector:'textarea'
+				}
+			);
+		</script>
+	</head>
+	<body>
+		<header class="row">
+			<div class="small-6 columns">
+				<h5>Backend/Administration Area</h5>
+			</div>
+			<nav class="small-6 columns">
+				<a href=" {{ route('site.index') }} ">Site Home</a> -
+				<a href="/backend">Backend Home</a> -
+				<a href="/backend/pages">Pages</a> -
+			</nav>
+		</header>
+		<hr>
+		<main class="row">
+			<aside class="large-4 columns">
+				@yield('sub-menu')
+			</aside>
+			@yield('content')
+		</main>		
+	</body>
+</html>
