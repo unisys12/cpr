6c7feb97217a640ce0edcab3c12e49416e49d455
Phillip Jackson <unisys12@gmail.com>
2015-01-01 12:22:13 -0600

    set-up base controller methods

--- app/controllers/FacilityController.php
+++ app/controllers/FacilityController.php
@@ -1,6 +1,13 @@
 <?php
 
+use Cpr\Storage\Facility\FacilityRepository as Facility;
+
 class FacilityController extends \BaseController {
+
+	public function __construct(Facility $facility)
+	{
+		$this->facility = $facility;
+	}
 
 	/**
 	 * Display a listing of the resource.
@@ -9,7 +16,8 @@ class FacilityController extends \BaseController {
 	 */
 	public function index()
 	{
-		//
+		$facilites = $this->facility->all();
+		return View::make('backend.facilites.index')->with('facilites', $facilites);
 	}
 
 
@@ -20,7 +28,7 @@ class FacilityController extends \BaseController {
 	 */
 	public function create()
 	{
-		//
+		return View::make('backend.facilites.new');
 	}
 
 
@@ -31,55 +39,91 @@ class FacilityController extends \BaseController {
 	 */
 	public function store()
 	{
-		//
+		$image_path = 'imgs/uploads/facilites/';
+
+		$data = [
+			'title' 				=> 	Input::get('title'),
+			'slug'					=>	Str::slug(Input::get('title')), 
+			'description'			=>	Input::get('description'),
+			'image_1'				=>	$image_path . Input::file('image_1')->getClientOriginalName(),
+			'image_1_description'	=>	Input::get('image_1_description'),
+			'image_2'				=>	$image_path . Input::file('image_2')->getClientOriginalName(),
+			'image_2_description'	=>	Input::get('image_2_description'),
+		];
+
+		Image::make(Input::file('image_1'))->save($data['image_1']);
+		Image::make(Input::file('image_2'))->save($data['image_2']);
+
+		$this->facility->store($data);
+		return Redirect::route('backend.facilites.index');
 	}
 
 
 	/**
 	 * Display the specified resource.
 	 *
-	 * @param  int  $id
+	 * @param  string  $slug
 	 * @return Response
 	 */
-	public function show($id)
+	public function show($slug)
 	{
-		//
+		$facilites = $this->facility->find($slug);
+		return View::make('backend.facilites.show')->with('facilites', $facilites);
 	}
 
 
 	/**
 	 * Show the form for editing the specified resource.
 	 *
-	 * @param  int  $id
+	 * @param  slug  $slug
 	 * @return Response
 	 */
-	public function edit($id)
+	public function edit($slug)
 	{
-		//
+		$facilites = $this->facility->find($slug);
+		return View::make('backend.facilites.edit')->with('facilites', $facilites);
 	}
 
 
 	/**
 	 * Update the specified resource in storage.
 	 *
-	 * @param  int  $id
+	 * @param  string  $slug
 	 * @return Response
 	 */
-	public function update($id)
+	public function update($slug)
 	{
-		//
+		$image_path = 'imgs/uploads/facilites/';
+
+		$data = [
+			'title' 				=> 	Input::get('title'),
+			'slug'					=>	Str::slug(Input::get('title')), 
+			'description'			=>	Input::get('description'),
+			'image_1'				=>	$image_path . Input::file('image_1')->getClientOriginalName(),
+			'image_1_description'	=>	Input::get('image_1_description'),
+			'image_2'				=>	$image_path . Input::file('image_2')->getClientOriginalName(),
+			'image_2_description'	=>	Input::get('image_2_description'),
+		];
+
+		Image::make(Input::file('image_1'))->save($data['image_1']);
+		Image::make(Input::file('image_2'))->save($data['image_2']);
+
+		$this->facility->update($data);
+		return Redirect::route('backend.facilites.index');
 	}
 
 
 	/**
 	 * Remove the specified resource from storage.
 	 *
-	 * @param  int  $id
+	 * @param  string  $slug
 	 * @return Response
 	 */
-	public function destroy($id)
+	public function destroy($slug)
 	{
-		//
+		$facilites = Facility::where('slug', $slug)->get();
+		$facilites->delete();
+		return Redirect::route('backend.facilites.index');
 	}
 
 
