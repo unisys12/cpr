9ea83f88c1bd038c3cfeab4173304a85810e0de5
Phillip Jackson <unisys12@gmail.com>
2015-01-01 10:41:21 -0600

    create facilities migration

---
+++ app/database/migrations/2015_01_01_163629_create_facilities_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFacilitiesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('facilities', function($table)
+		{
+			$table->increments('id');
+			$table->string('title');
+			$table->string('slug');
+			$table->text('description');
+			$table->string('image_1');
+			$table->string('image_1_description');
+			$table->string('image_2');
+			$table->string('image_2_description');
+			$table->timestamps();
+			$table->softDeletes();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
