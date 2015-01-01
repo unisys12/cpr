a5f69c792ca691033c41c7fef587d8a192ea34bf
Phillip Jackson <unisys12@gmail.com>
2015-01-01 12:40:21 -0600

    create edit page

---
+++ app/views/backend/facility/edit.blade.php
@@ -0,0 +1,52 @@
+@extends('templates.admin')
+
+@section('title')
+Edit a Existing Facility
+@stop
+
+@section('header')
+	<div class="small-6 columns">
+		<h5>Edit an Existing Facility</h5>
+	</div>
+@stop
+
+@section('sub-menu')
+	<header>
+		<h4>Faclities</h4>
+	</header>
+	<nav>
+		<ul>
+			<li><a href=" {{ route('backend.facility.index') }} ">Facilities Home</a></li>
+			<li><a href="{{ route('backend.facility.create') }}">Add a New Facility</a></li>
+		</ul>
+	</nav>
+@stop
+
+@section('content')
+	<div class="small-12 large-8 columns">
+		<p class="message">
+			Edit the following page, then click the "Save Edits" button below. To cancel, just click your browsers back button.
+		</p>
+		{{ Form::open(['route' => 'backend.facility.update', 'method' => 'put']) }}
+		@foreach ($facilities as $item)
+		
+		{{ Form::label('title', 'Give a Title to Your Page') }}
+		{{ Form::text('title', $item->title) }}
+
+		{{ Form::label('description', 'Describe this Facility') }}
+		{{ Form::text('description', $item->description) }}
+
+		{{ Form::label('image_1', 'Choose the First Image to Display') }}
+		{{ Form::file('image_1', ['required' => true]) }}
+		{{ Form::text('image_1_description', $item->image_1_description) }}
+
+		{{ Form::label('image_2', 'Choose the Second Image to Display') }}
+		{{ Form::file('image_2', ['required' => true]) }}
+		{{ Form::text('image_2_description', $item->image_2_description) }}
+
+		@endforeach
+		{{ Form::submit('Save Edits') }}
+
+		{{ Form::close() }}
+	</div>
+@stop
