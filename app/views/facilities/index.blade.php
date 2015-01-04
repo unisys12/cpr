dc9034b410fb037d6922aa153833b046bdebb864
Phillip Jackson <unisys12@gmail.com>
2015-01-03 18:31:18 -0600

    add new facilities view dir and index file

---
+++ app/views/facilities/index.blade.php
@@ -0,0 +1,31 @@
+@extends('templates.site')
+
+@section('title')
+	@foreach ($data as $element)
+		{{ $element->title }}
+	@endforeach
+@stop
+
+@section('content')
+	<div class="large-8 columns">		
+		@foreach ($data as $element)
+			<header>
+				<h2>{{ $element->name }}</h2>
+			</header>
+			<figure>
+				<img src="{{ asset($element->image_1) }}" alt="{{ $element->image_1_description }}">
+				<figcaptions>
+					<small>
+						{{ $element->description }}
+					</small>
+				</figcaption>
+			</figure>
+		@endforeach
+	</div>
+@stop
+
+@section('side-nav')
+@foreach (Page::all() as $page)
+	<li><a href="/{{ $page->slug }}">{{ $page->title }}</a></li>
+@endforeach
+@stop
