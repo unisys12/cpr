98824d29e80909fa13ac37d071b551c477e3a992
Phillip Jackson <unisys12@gmail.com>
2015-01-03 18:52:24 -0600

    update path to css file to use asset method

--- app/views/templates/site.blade.php
+++ app/views/templates/site.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>@yield('title', 'Corinth Alcorn Parks and Recreation')</title>
-    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
     <script src=""></script>
   </head>
 <body class="background">
