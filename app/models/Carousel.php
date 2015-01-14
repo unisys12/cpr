cd4258d9ab3235d0bdec59267b08c09d4a71bba0
Phillip Jackson <unisys12@gmail.com>
2015-01-13 20:14:44 -0600

    create validate method

--- app/models/Carousel.php
+++ app/models/Carousel.php
@@ -24,5 +24,31 @@ class Carousel extends Eloquent {
 	 * @var string
 	 */
 	protected $guarded = ['_token'];
+
+	/**
+	 * Validate Method to inspect your inputs
+	 * 
+	 * @var string
+	 *
+	 * @return bool
+	 */
+	public function validate($input)
+	{
+
+		$rules = [
+
+		'name'					=>	'required|min:5|unique:facilities',
+		'description'			=>	'required|min:10|max:255',
+		'image_1_description'	=> 	'required|min:5|max:25',
+		'image_2_description'	=> 	'required|min:5|max:25',
+		'image_3_description'	=> 	'required|min:5|max:25',
+
+		];
+
+		$attempt = Validator::make($input, $rules);
+
+		return $attempt;
+
+	}
 	
 }
