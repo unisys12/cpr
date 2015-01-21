a30f825de948312bcef9e3f558aed94e783eb210
Phillip Jackson <unisys12@gmail.com>
2015-01-21 08:05:34 -0600

    remove spaces

--- app/views/backend/pages/show.blade.php
+++ app/views/backend/pages/show.blade.php
@@ -16,7 +16,7 @@
 
 @section('content')
 	<div class="small-12 large-8 columns">	
-		@foreach ($page as $section)	
+		@foreach($page as $section)	
 			<p>{{ $section->title }}</p>
 			<h4>{{ $section->header }}</h4>
 			<p>{{ $section->content }}</p>
