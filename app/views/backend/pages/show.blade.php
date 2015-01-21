5680e3a7eb4406181b03574236e0bf22d79de56f
Phillip Jackson <unisys12@gmail.com>
2015-01-21 08:43:15 -0600

    remove unneeded div

--- app/views/backend/pages/show.blade.php
+++ app/views/backend/pages/show.blade.php
@@ -12,11 +12,11 @@
 @stop
 
 @section('content')
-	<div class="small-12 large-8 columns">	
-		@foreach($page as $section)	
-			<p>{{ $section->title }}</p>
-			<h4>{{ $section->header }}</h4>
-			<p>{{ $section->content }}</p>
-		@endforeach
-	</div>
+	
+	@foreach($page as $section)	
+		<p>{{ $section->title }}</p>
+		<h4>{{ $section->header }}</h4>
+		<p>{{ $section->content }}</p>
+	@endforeach
+
 @stop
