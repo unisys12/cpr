81c92bb104cd463fad19d017fee4db75946dd6fa
Phillip Jackson <unisys12@gmail.com>
2015-01-27 20:38:20 -0600

    remove unwanted to div

--- app/views/backend/facility/edit.blade.php
+++ app/views/backend/facility/edit.blade.php
@@ -23,30 +23,30 @@ Edit a Existing Facility
 @stop
 
 @section('content')
-	<div class="small-12 large-8 columns">
-		<p class="message">
-			Edit the following page, then click the "Save Edits" button below. To cancel, just click your browsers back button.
-		</p>
-		@foreach ($facilities as $facility)
-		{{ Form::open(['route' => ['backend.facility.update', $facility->slug], 'method' => 'PUT', 'files' => true]) }}
-		
-		{{ Form::label('name', 'Facility Name') }}
-		{{ Form::text('name', $facility->name) }}
+	
+	<p class="message">
+		Edit the following page, then click the "Save Edits" button below. To cancel, just click your browsers back button.
+	</p>
+	@foreach ($facilities as $facility)
+	{{ Form::open(['route' => ['backend.facility.update', $facility->slug], 'method' => 'PUT', 'files' => true]) }}
+	
+	{{ Form::label('name', 'Facility Name') }}
+	{{ Form::text('name', $facility->name) }}
 
-		{{ Form::label('description', 'Describe this Facility') }}
-		{{ Form::text('description', $facility->description) }}
+	{{ Form::label('description', 'Describe this Facility') }}
+	{{ Form::text('description', $facility->description) }}
 
-		{{ Form::label('image_1', 'Choose the First Image to Display') }}
-		{{ Form::file('image_1', ['required' => true]) }}
-		{{ Form::text('image_1_description', $facility->image_1_description) }}
+	{{ Form::label('image_1', 'Choose the First Image to Display') }}
+	{{ Form::file('image_1', ['required' => true]) }}
+	{{ Form::text('image_1_description', $facility->image_1_description) }}
 
-		{{ Form::label('image_2', 'Choose the Second Image to Display') }}
-		{{ Form::file('image_2', ['required' => true]) }}
-		{{ Form::text('image_2_description', $facility->image_2_description) }}
+	{{ Form::label('image_2', 'Choose the Second Image to Display') }}
+	{{ Form::file('image_2', ['required' => true]) }}
+	{{ Form::text('image_2_description', $facility->image_2_description) }}
 
-		@endforeach
-		{{ Form::submit('Save Edits') }}
+	@endforeach
+	{{ Form::submit('Save Edits') }}
 
-		{{ Form::close() }}
-	</div>
+	{{ Form::close() }}
+	
 @stop
