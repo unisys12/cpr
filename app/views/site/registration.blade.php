b5a8535546a58f2923b8e71f5281b13beac5004a
Phillip Jackson <unisys12@gmail.com>
2015-02-07 09:12:54 -0600

    add registration view

---
+++ app/views/site/registration.blade.php
@@ -0,0 +1,43 @@
+@extends('templates.site')
+
+@section('title')
+Sports Registration Information
+@stop
+
+@section('content')
+	<article class="reg-info">
+		<h2>2015 Youth Baseball &amp; Girls Fast Pitch Registration</h2>
+		<p><b>Registration will end February 20th</b></p>
+		<p>Cost: $45.00/child</p>
+		<hr>
+		<h2>2015 Adult Softball Registration</h2>
+		<p>Registration will be held February 17th - March 6th</p>
+		<p>Cost: $350.00/team (County Residents)</p>
+		<p>Cost: $400.00/team (Out of County Residents)</p>
+		<hr>
+		<p>You can register at our office:</p>
+		<ul>
+			<li>309 South Parkway</li>
+			<li>Corinth, MS 38834</li>
+			<li>Office Hours: Monday - Friday; 8:00am - 5:00pm</li>
+		</ul>
+		<p><i>Proof of residency is required</i></p>
+		<hr>
+		<h2>Observations:</h2>
+		<p>Tentatively scheduled for Saturday, February 28th</p>
+		<ul>
+			<li>Girls 12U, Boys 11-12: 8:30am</li>
+			<li>Girls 10U, Boys 9-10 9:45 am</li>
+			<li>Girls 8U, Boys 7-8: 11am</li>
+			<li>Girls 6U, Boys 6: 12pm</li>			
+			<li>Draft Only for Girls 14U &amp; 16U, Boys 13-15</li>
+			<li>T-Ball: No Observations</li>
+		</ul>
+	</article>
+@stop
+
+@section('side-nav')
+@foreach (Page::all() as $page)
+<li><a href="/{{ $page->slug }}">{{ $page->title }}</a></li>
+@endforeach
+@stop
