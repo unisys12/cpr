5c6af1c1f6bfc93f7f58d5412012c529206d30c6
Phillip Jackson <unisys12@gmail.com>
2015-02-08 08:52:31 -0600

    configure proper env seperation

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,8 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('Phillip-PC'),
+	'local' => array('homestead'),
+	'production' => array('production')
 
 ));
 
