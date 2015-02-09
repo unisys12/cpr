9a27740fa6e9982c6c86d3fb6150b3946f51a110
Phillip Jackson <unisys12@gmail.com>
2015-02-08 19:58:02 -0600

    create staff migration

---
+++ app/database/migrations/2015_02_09_015333_create_staff_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStaffTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		
+		Schema::create('staff', function($table)
+		{
+			$table->increaments('id');
+			$table->string('name');
+			$table->string('title');
+			$table->string('email');
+			$table->text('summary');
+			$table->timestamps();
+			$table->softDeletes();
+		});
+
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		
+		Schema::drop('staff');
+
+	}
+
+}
