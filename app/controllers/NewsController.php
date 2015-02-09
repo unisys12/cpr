b9717e449fe249aec17cb5131925fd0862019f57
Phillip Jackson <unisys12@gmail.com>
2015-02-08 19:07:38 -0600

    finish news controller

--- app/controllers/NewsController.php
+++ app/controllers/NewsController.php
@@ -4,6 +4,7 @@ class NewsController extends \BaseController {
 
 	public function __construct(News $news)
 	{
+		$this->beforeFilter('auth');
 		$this->news = $news;
 	}
 
@@ -14,7 +15,9 @@ class NewsController extends \BaseController {
 	 */
 	public function index()
 	{
-		//
+		
+		return View::make('backend.news.index')->with('news', $this->news->all());
+
 	}
 
 
@@ -25,7 +28,9 @@ class NewsController extends \BaseController {
 	 */
 	public function create()
 	{
-		//
+		
+		return View::make('backend.news.new');
+
 	}
 
 
@@ -36,55 +41,96 @@ class NewsController extends \BaseController {
 	 */
 	public function store()
 	{
-		//
+		
+		$data = [
+			'title' 	=> Input::get('title'),
+			'slug'		=> Str::slug(Input::get('title')),
+			'header'	=> Input::get('header'),
+			'content'	=> Input::get('content')
+		];
+
+		$attempt = $this->news->validate($data);
+
+		if( $attempt->fails() ){
+			return Redirect::route('backend.news.create')->with('errors', $attempt->messages())->withInput();
+		}
+
+		$this->news->create($data);
+		return Redirect::route('backend.news.index');
+
 	}
 
 
 	/**
 	 * Display the specified resource.
 	 *
-	 * @param  int  $id
+	 * @param  string  $slug
 	 * @return Response
 	 */
-	public function show($id)
+	public function show($slug)
 	{
-		//
+		return View::make('backend.news.show')->with('news', $this->news->where('slug', $slug)->get());
 	}
 
 
 	/**
 	 * Show the form for editing the specified resource.
 	 *
-	 * @param  int  $id
+	 * @param  string  $slug
 	 * @return Response
 	 */
-	public function edit($id)
+	public function edit($slug)
 	{
-		//
+		return View::make('backend.news.edit')->with('news', $this->news->where('slug', $slug)->get());
 	}
 
 
 	/**
 	 * Update the specified resource in storage.
 	 *
-	 * @param  int  $id
+	 * @param  string  $slug
 	 * @return Response
 	 */
-	public function update($id)
+	public function update($slug)
 	{
-		//
+		
+		$data = [
+			'title' 	=> Input::get('title'),
+			'slug'		=> Str::slug(Input::get('title')),
+			'header'	=> Input::get('header'),
+			'content'	=> Input::get('content')
+		];
+
+		$attempt = $this->news->validate($data);
+
+		if( $attempt->fails() ){
+			return Redirect::route('backend.news.create')->with('errors', $attempt->messages())->withInput();
+		}
+
+		$existing = $this->news->where('slug', $slug);
+		
+		$existing->update($data);
+
+		return Redirect::route('backend.news.index');
+
 	}
 
 
 	/**
-	 * Remove the specified resource from storage.
+	 * Remove the specified announcement from storage.
 	 *
-	 * @param  int  $id
+	 * @param  string  $slug
 	 * @return Response
 	 */
-	public function destroy($id)
+	public function destroy($slug)
 	{
-		//
+		
+		$news = $this->news->where('slug', $slug);
+
+		$news->delete();
+
+		return Redirect::route('backend.news.index');
+
 	}
 
 
