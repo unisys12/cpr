246f2023382350933163c7b36cb01ce809a17a91
Phillip Jackson <unisys12@gmail.com>
2015-02-09 07:22:21 -0600

    create contact view

---
+++ app/views/site/contact.blade.php
@@ -0,0 +1,23 @@
+@extends('templates.site')
+
+@section('title')
+Contact Information
+@stop
+
+@section('content')
+
+<h3>Contact Us</h3>
+<ul>
+	<li>Office Hours: Monday - Friday 8am - 5pm</li>
+	<li>Phone: <a href="tel://(662)286-3067" class="tele">(662)286-3067</a></li>
+	<li>Fax: (662)286-1310</li>
+	<li>Mailing Address: P.O. Box 1372, Corinth, MS 38835</li>
+	<li>Shipping Address: 309 S. Parkway St, Corinth, MS 38834</li>
+</ul>
+@stop
+
+@section('side-nav')
+@foreach (Page::all() as $page)
+<li><a href="/{{ $page->slug }}">{{ $page->title }}</a></li>
+@endforeach
+@stop
